936912f32bce22cbae4d4c5695f76d734ee68b17
Felix Staudigl <fstaudigl@imr.rwth-aachen.de>
2017-05-13 19:43:42 +0200

    push

--- examples/ros_nodes/show_objects_node.cpp
+++ examples/ros_nodes/show_objects_node.cpp
@@ -89,16 +89,17 @@ int main(int argc, char* argv[]) {
   ros::init(argc, argv, "show_objects_node");
   ros::NodeHandle nh;
 
-  string topic_clouds = "/velodyne_points";
+  string topic_clouds = "/assembled_laser";
   string topic_laser = "/hokuyo/scan/raw";
   string topic_odom = "";
 
-  LaserRosSubscriber subscriber(&nh, *proj_params_ptr, topic_laser, topic_odom=topic_odom); //CloudOdomRosSubscriber
+  // LaserRosSubscriber subscriber(&nh, *proj_params_ptr, topic_laser, topic_odom=topic_odom); //CloudOdomRosSubscriber
+  CloudOdomRosSubscriber subscriber(&nh, *proj_params_ptr, topic_clouds);
   Visualizer visualizer;
   visualizer.show();
 
   int min_cluster_size = 50; // 20
-  int max_cluster_size = 100000;
+  int max_cluster_size = 100000;//100000
 
   int smooth_window_size = 7;
   Radians ground_remove_angle = 7_deg;
@@ -109,13 +110,13 @@ int main(int argc, char* argv[]) {
   ClustererT clusterer(angle_tollerance, min_cluster_size, max_cluster_size);
   clusterer.SetDiffType(DiffFactory::DiffType::ANGLES);
 
-  subscriber.AddClient(&depth_ground_remover);
-  depth_ground_remover.AddClient(&clusterer);
-  depth_ground_remover.AddClient(&visualizer);  ///
-  // subscriber.AddClient(&clusterer);
+  // subscriber.AddClient(&depth_ground_remover);
+  // depth_ground_remover.AddClient(&clusterer);
+  // depth_ground_remover.AddClient(&visualizer);  ///
+  subscriber.AddClient(&clusterer);
   clusterer.AddClient(visualizer.object_clouds_client());
    ///
-  //subscriber.AddClient(&visualizer);
+  clusterer.AddClient(&visualizer);
 
   fprintf(stderr, "INFO: Running with angle tollerance: %f degrees\n",
           angle_tollerance.ToDegrees());
